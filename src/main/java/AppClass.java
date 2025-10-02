3ec071c6fe8a11ccae0ea1feea7eeabeb2bb4647
Samyak475 <sasamyakjy25@gmail.com>
2025-10-02 13:30:55 +0530

    sd

--- src/main/java/AppClass.java
+++ src/main/java/AppClass.java
@@ -25,12 +25,19 @@ public class AppClass  {
                 .GET().build();
 
         HttpClient httpClient = HttpClient.newHttpClient();
-        HttpResponse<String> response ;
+        HttpResponse<String> response = null;
         try {
             response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        } catch (InterruptedException | IOException e) {
-            throw new RuntimeException(e);
+//            System.out.println(response.statusCode());
+            if(response.statusCode()==404){
+                System.out.println("bad username");
+                return;
+            }
+
+        } catch (Exception e) {
+             e.printStackTrace();
         }
+
 
         ObjectMapper obj = new ObjectMapper();
         List<GitExtract> data = obj.readValue(response.body(), new TypeReference<List<GitExtract>>() {
@@ -45,6 +52,7 @@ public class AppClass  {
 
         if(dt.getType().compareTo("PushEvent")==0 && dt.getPayload()!=null &&dt.getPayload().getCommits() !=null){
             System.out.println("Here for repository "+dt.getRepo().getName()+"Printing all the commits ***************************");
+            System.out.println("Total Commit done by user "+ dt.getPayload().getCommits().size());
            for(Commits commits:dt.getPayload().getCommits()){
                System.out.println("User "+dt.getActor().getLogin()+" has done commit with message "+ commits.getMessage());
            }
